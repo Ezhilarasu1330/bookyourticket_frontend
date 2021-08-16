571fec7b184a540ff0b60b290c8cba7fafd84560
Ezhilarasu <ezhil9841@gmail.com>
2021-08-16 20:26:18 +0530

    Fix : Added Price Information

--- src/components/screens/ScreenSeats.js
+++ src/components/screens/ScreenSeats.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Card } from 'react-bootstrap'
+import { Row, Col, Card, ListGroup, Button } from 'react-bootstrap'
 import axios from 'axios'
 import Loader from '../Loader'
 import Message from '../Message'
@@ -13,6 +13,8 @@ const ScreenSeats = ({ history, match }) => {
 
     const [loading, setLoading] = useState('')
     const [error, setError] = useState('')
+    const [ticketCount, setTicketCount] = useState(0)
+    const [price, setPrice] = useState(0)
     // Notification
     const [notify, setNotify] = useState({ isOpen: false, message: '', type: '' })
     const [data, setData] = useState({ screenSeatInfo: [], theatreName: '', movieName: '', showStartTime: '' });
@@ -37,18 +39,27 @@ const ScreenSeats = ({ history, match }) => {
 
     const selectSeatHandler = (event) => {
 
+        // if (event.target.classList.contains('selected')) {
+        //     console.log(event.target.className.replace("selected", "").trim())
+        // }
+
         var seats = document.getElementsByClassName("selected");
-        console.log('seats ::: ', seats)
         if (seats.length == 7) {
             setNotify({
                 isOpen: true, message: 'You can able to select only 6 seats', type: 'error'
             })
         }
         else {
+            setTicketCount(seats.length)
+            setPrice(seats.length * 120)
             if (!event.target.classList.contains('occupied') && !event.target.classList.contains('selected')) {
                 event.target.classList.add("selected");
             }
         }
+    }
+
+    const bookTicketCartHandler = () => {
+        console.log("Book Tickets");
     }
 
     return (
@@ -59,49 +70,90 @@ const ScreenSeats = ({ history, match }) => {
                     (<Message variant='danger'>{error}</Message>)
                     :
                     (<>
-                        <h1>{data.theatreName}</h1>
-                        <Row>
-                            <Col md={6} className='my-3'>
-                                <h5>{data.movieName}</h5>
-                            </Col>
-                            <Col md={6} className='my-3'>
-                                <h5> Show Time : {moment(data.showStartTime).format("HH:mm a")}</h5>
-                            </Col>
-                        </Row>
-
-
-                        <ul className="showcase">
-                            <li>
-                                <div id="seat" className="seat" />
-                                <small className="status" style={{ fontSize: '1em' }}>N/A</small>
-                            </li>
-                            <li>
-                                <div id="seat" className="seat selected" />
-                                <small className="status" style={{ fontSize: '1em' }}>Selected</small>
-                            </li>
-                            <li>
-                                <div id="seat" className="seat occupied" />
-                                <small className="status" style={{ fontSize: '1em' }}>Occupied</small>
-                            </li>
-                        </ul>
-
-                        <br></br>
-                        <br></br>
-                        <br></br>
-
-                        <div style={{ perspective: '1000px' }}>
-                            <div className="screen"></div>
-                        </div>
 
                         <Row>
-                            {data.screenSeatInfo ? data.screenSeatInfo.map((seatInfo) => (
-                                <div
-                                    id={"seat_" + seatInfo.screenSeatId}
-                                    key={seatInfo.screenSeatId}
-                                    className={"seat " + (seatInfo.booked ? 'occupied' : '')}
-                                    onClick={(e) => selectSeatHandler(e)}>
+                            <Col md={9}>
+                                <h1>{data.theatreName}</h1>
+                                <Row>
+                                    <Col md={6} className='my-3'>
+                                        <h5>{data.movieName}</h5>
+                                    </Col>
+                                    <Col md={6} className='my-3'>
+                                        <h5> Show Time : {moment(data.showStartTime).format("HH:mm a")}</h5>
+                                    </Col>
+                                </Row>
+
+                                <ul className="showcase">
+                                    <li>
+                                        <div id="seat" className="seat" />
+                                        <small className="status" style={{ fontSize: '1em' }}>N/A</small>
+                                    </li>
+                                    <li>
+                                        <div id="seat" className="seat selected" />
+                                        <small className="status" style={{ fontSize: '1em' }}>Selected</small>
+                                    </li>
+                                    <li>
+                                        <div id="seat" className="seat occupied" />
+                                        <small className="status" style={{ fontSize: '1em' }}>Occupied</small>
+                                    </li>
+                                </ul>
+
+                                <br></br>
+                                <br></br>
+                                <br></br>
+
+                                <div style={{ perspective: '1000px' }}>
+                                    <div className="screen"></div>
                                 </div>
-                            )) : <h1></h1>}
+
+                                <Row>
+                                    {data.screenSeatInfo ? data.screenSeatInfo.map((seatInfo) => (
+                                        <div
+                                            id={"seat_" + seatInfo.screenSeatId}
+                                            key={seatInfo.screenSeatId}
+                                            className={"seat " + (seatInfo.booked ? 'occupied' : '')}
+                                            onClick={(e) => selectSeatHandler(e)}>
+                                        </div>
+                                    )) : <h1></h1>}
+                                </Row>
+                            </Col>
+
+                            <Col md={3}>
+                                <div className="priceInfo">
+                                    <Card>
+                                        <ListGroup variant='flush'>
+
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>No of seats selected :</Col>
+                                                    <Col>
+                                                        {ticketCount}
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Price:</Col>
+                                                    <Col>
+                                                        {price}
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+
+                                            <ListGroup.Item>
+                                                <Button
+                                                    onClick={bookTicketCartHandler}
+                                                    className='btn-block'
+                                                    type='button'
+                                                    disabled={ticketCount === 0 || ticketCount > 6}
+                                                >Book Tickets</Button>
+                                            </ListGroup.Item>
+                                        </ListGroup>
+                                    </Card>
+                                </div>
+
+                            </Col>
                         </Row>
                     </>)
             }
